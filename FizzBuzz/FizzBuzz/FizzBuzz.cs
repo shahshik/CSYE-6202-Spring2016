1d4223e3c63c031d7e6cba25642a6f445e9c3a0e
Shikha Shah <shah.shik@husky.neu.edu>
2016-02-13 08:43:44 +0530

    Commit First

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -6,9 +6,31 @@
 		{
 			string result = number.ToString();
 
-			// your solution/implementation should go in here
+            // your solution/implementation should go in here
+            if (i == 0)
+            {
+                output = "0";
+            }
+            else if (i % 3 == 0 && i % 5 == 0)
+            {
+                output = "FizzBuzz";
+            }
+            else if (i % 5 == 0)
+            {
+                output = "Buzz";
+            }
+            else if (i % 3 == 0)
+            {
 
-			return result;
+                output = "Fizz";
+            }
+            else
+            {
+                output = i.ToString();
+            }
+
+            return output;
+            return result;
 		}
 	}
 }
