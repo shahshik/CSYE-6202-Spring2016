ccfba7cb8ca42c6b0ee5b74e0bb9f84516785ba3
Shikha Shah <shah.shik@husky.neu.edu>
2016-04-05 01:31:11 +0530

    Commit

---
+++ win/Srudent Registration/Srudent Registration/IStudentRegistrationForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Srudent_Registration
+{
+    interface IStudentRegistrationForm : IView
+    {
+        event EventHandler DataChanged;
+    }
+}
---
+++ win/Srudent Registration/Srudent Registration/StudentRegistration.Domain/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Srudent_Registration.StudentRegistration.Domain
+{
+    public class Student
+    { 
+        public string StudentID { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string EnrollmentType { get; set; }
+
+        public Student(string studentId, string firstName, string lastName, string department,string enrollmentType)
+        {
+            StudentID = studentId;
+            FirstName = firstName;
+            LastName = lastName;
+            Department = department;
+            EnrollmentType = enrollmentType;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("StudentID: {0}, FirstName: {1}, LastName: {2}, Department: {3}, EnrollmentType: {4}", StudentID, FirstName, LastName, Department, EnrollmentType);
+        }
+    }
+}
