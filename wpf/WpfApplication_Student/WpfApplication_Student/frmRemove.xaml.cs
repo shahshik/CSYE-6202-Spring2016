18a7f0490f91c4c383d1edf6f2ee57444c412f3e
Shikha Shah <shah.shik@husky.neu.edu>
2016-04-11 21:06:12 +0530

    commit2

---
+++ wpf/WpfApplication_Student/WpfApplication_Student/frmRemove.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication_Student
+{
+    Student s1;
+    String v;
+
+    /// Interaction logic for frmRemove.xaml
+    /// </summary>
+    public partial class frmRemove : Window
+    {
+        public frmRemove(Student s)
+        {
+            InitializeComponent();
+            s1 = s;
+            txtStudentId.Text = s1.StudentID;
+            txtFirstName.Text = s1.FirstName;
+            txtLastName.Text = s1.LastName;
+            cmbDepartment.Text = s1.Department;
+            v = s1.EnrollmentType;
+            if (v.Equals("Part Time"))
+            {
+                btnPartTime.Checked = true;
+            }
+            else
+            {
+
+                btnFullTime.Checked = true;
+            }
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Are you sure you want to remove this student", "Are you sure you want to remove this student?", MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                //Student.mockStudentList.Remove(s1);
+                this.Close();
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+    }
+}
